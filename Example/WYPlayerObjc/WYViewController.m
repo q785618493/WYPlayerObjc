d87fda0ba786dd200852ee0d6279c9ca8ddb197e
q785618493 <785618493@qq.com>
2023-02-28 18:04:01 +0800

    直播源

--- Example/WYPlayerObjc/WYViewController.m
+++ Example/WYPlayerObjc/WYViewController.m
@@ -30,130 +30,130 @@
     
     /* 直播源，自己慢慢试哪个能用
      [
-       {
-         "address": "http://117.148.179.136/PLTV/88888888/224/3221231468/index.m3u8",
-         "img": "http://cdn.youlxodxka.live/img/1.jpg",
-         "title": "CCTV1综合"
-       },
-       {
-         "address": "http://117.148.179.156/PLTV/88888888/224/3221232362/index.m3u8",
-         "img": "http://cdn.youlxodxka.live/img/2.jpg",
-         "title": "CCTV2财经"
-       },
-       {
-         "address": "http://shbu.live.bestvcdn.com.cn:8080/live/program/live/cctv3hd/2300000/mnf.m3u8",
-         "img": "http://cdn.youlxodxka.live/img/3.jpg",
-         "title": "cctv3综艺"
-       },
-       {
-         "address": "http://39.135.138.60:18890/PLTV/88888910/224/3221225621/index.m3u8",
-         "img": "http://cdn.youlxodxka.live/img/4.jpg",
-         "title": "CCTV4国际"
-       },
-       {
-         "address": "http://117.148.179.165/PLTV/88888888/224/3221231702/index.m3u8",
-         "img": "http://cdn.youlxodxka.live/img/5.jpg",
-         "title": "CCTV5体育"
-       },
-       {
-         "address": "http://117.148.179.136/PLTV/88888888/224/3221231459/1.m3u8",
-         "img": "http://cdn.youlxodxka.live/img/5p.jpg",
-         "title": "CCTV5+体育"
-       },
-       {
-         "address": "http://39.134.65.175/PLTV/88888888/224/3221225814/index.m3u8",
-         "img": "http://cdn.youlxodxka.live/img/6.jpg",
-         "title": "CCTV6电影"
-       },
-       {
-         "address": "http://shbu.live.bestvcdn.com.cn:8080/live/program/live/cctv7/1300000/mnf.m3u8",
-         "img": "http://cdn.youlxodxka.live/img/7.jpg",
-         "title": "CCTV7军农"
-       },
-       {
-         "address": "http://39.135.138.58:18890/PLTV/88888888/224/3221225631/index.m3u8",
-         "img": "http://cdn.youlxodxka.live/img/8.jpg",
-         "title": "CCTV8电视剧"
-       },
-       {
-         "address": "http://117.148.179.162/PLTV/88888888/224/3221231697/index.m3u8",
-         "img": "http://cdn.youlxodxka.live/img/9.jpg",
-         "title": "CCTV9纪录"
-       },
-       {
-         "address": "http://shbu.live.bestvcdn.com.cn:8080/live/program/live/cctv10hd/4000000/mnf.m3u8",
-         "img": "http://cdn.youlxodxka.live/img/10.jpg",
-         "title": "CCTV10科技"
-       },
-       {
-         "address": "http://shbu.live.bestvcdn.com.cn:8080/live/program/live/cctv11/1300000/mnf.m3u8",
-         "img": "http://cdn.youlxodxka.live/img/11.jpg",
-         "title": "CCTV11戏曲"
-       },
-       {
-         "address": "http://shbu.live.bestvcdn.com.cn:8080/live/program/live/cctv12/1300000/mnf.m3u8",
-         "img": "http://cdn.youlxodxka.live/img/12.jpg",
-         "title": "CCTV12社会与法"
-       },
-       {
-         "address": "http://111.40.196.30/PLTV/88888888/224/3221225504/index.m3u8",
-         "img": "http://cdn.youlxodxka.live/img/13.jpg",
-         "title": "CCTV13新闻"
-       },
-       {
-         "address": "http://shbu.live.bestvcdn.com.cn:8080/live/program/live/cctvsehd/4000000/mnf.m3u8",
-         "img": "http://cdn.youlxodxka.live/img/14.jpg",
-         "title": "CCTV14少儿"
-       },
-       {
-         "address": "http://shbu.live.bestvcdn.com.cn:8080/live/program/live/cctvyy/1300000/mnf.m3u8",
-         "img": "http://cdn.youlxodxka.live/img/15.jpg",
-         "title": "CCTV15音乐"
-       },
-       {
-         "address": "http://117.148.179.167/PLTV/88888888/224/3221231772/index.m3u8",
-         "img": "http://cdn.youlxodxka.live/img/17.jpg",
-         "title": "CCTV17农村"
-       },
-       {
-         "address": "http://hw-m-l.cztv.com/channels/lantian/channel01/1080p.m3u8",
-         "img": "http://cdn.youlxodxka.live/img/zj.jpg",
-         "title": "浙江卫视"
-       },
-       {
-         "address": "http://shbu.live.bestvcdn.com.cn:8080/live/program/live/hddfws/2300000/mnf.m3u8",
-         "img": "http://cdn.youlxodxka.live/img/df.jpg",
-         "title": "东方卫视"
-       },
-       {
-         "address": "http://shbu.live.bestvcdn.com.cn:8080/live/program/live/hnwshd/2300000/mnf.m3u8",
-         "img": "http://cdn.youlxodxka.live/img/hn.jpg",
-         "title": "湖南卫视"
-       },
-       {
-         "address": "http://shbu.live.bestvcdn.com.cn:8080/live/program/live/jswshd/2300000/mnf.m3u8",
-         "img": "http://cdn.youlxodxka.live/img/js.jpg",
-         "title": "江苏卫视"
-       },
-       {
-         "address": "http://live.xmcdn.com/live/1040/64.m3u8",
-         "img": "http://cdn.youlxodxka.live/img/dt.png",
-         "title": "环球资讯广播"
-       },
-       {
-         "address": "http://live.xmcdn.com/live/1043/64.m3u8",
-         "img": "http://cdn.youlxodxka.live/img/dt.png",
-         "title": "中文环球广播"
-       },
-       {
-         "address": "http://ls.qingting.fm/live/386.m3u8",
-         "img": "http://cdn.youlxodxka.live/img/dt.png",
-         "title": "中国之声"
-       }
-     ]
+         {
+           "address" : "http://hwrr.jx.chinamobile.com:8080/PLTV/88888888/224/3221225618/index.m3u8",
+           "img" : "http://cdn.youlxodxka.live/img/1.jpg",
+           "title" : "CCTV1综合"
+         },
+         {
+           "address" : "http://hwrr.jx.chinamobile.com:8080/PLTV/88888888/224/3221225619/index.m3u8",
+           "img" : "http://cdn.youlxodxka.live/img/2.jpg",
+           "title" : "CCTV2财经"
+         },
+         {
+           "address" : "http://hwrr.jx.chinamobile.com:8080/PLTV/88888888/224/3221225647/index.m3u8",
+           "img" : "http://cdn.youlxodxka.live/img/3.jpg",
+           "title" : "cctv3综艺"
+         },
+         {
+           "address" : "http://39.135.138.60:18890/PLTV/88888910/224/3221225621/index.m3u8",
+           "img" : "http://cdn.youlxodxka.live/img/4.jpg",
+           "title" : "CCTV4国际"
+         },
+         {
+           "address" : "https://pull-l3-cny.douyincdn.com/live/6c96718e1544173e6f6f3041ccaf1e6d.m3u8",
+           "img" : "http://cdn.youlxodxka.live/img/5.jpg",
+           "title" : "CCTV5体育"
+         },
+         {
+           "address" : "http://39.135.138.58:18890/PLTV/88888888/224/3221225649/index.m3u8",
+           "img" : "http://cdn.youlxodxka.live/img/5p.jpg",
+           "title" : "CCTV5+体育"
+         },
+         {
+           "address" : "http://39.135.138.58:18890/PLTV/88888888/224/3221225632/index.m3u8",
+           "img" : "http://cdn.youlxodxka.live/img/6.jpg",
+           "title" : "CCTV6电影"
+         },
+         {
+           "address" : "http://39.135.138.58:18890/PLTV/88888888/224/3221225624/index.m3u8",
+           "img" : "http://cdn.youlxodxka.live/img/7.jpg",
+           "title" : "CCTV7军农"
+         },
+         {
+           "address" : "http://39.135.138.58:18890/PLTV/88888888/224/3221225631/index.m3u8",
+           "img" : "http://cdn.youlxodxka.live/img/8.jpg",
+           "title" : "CCTV8电视剧"
+         },
+         {
+           "address" : "http://39.135.138.58:18890/PLTV/88888888/224/3221225626/index.m3u8",
+           "img" : "http://cdn.youlxodxka.live/img/9.jpg",
+           "title" : "CCTV9纪录"
+         },
+         {
+           "address" : "http://39.135.138.58:18890/PLTV/88888888/224/3221225627/index.m3u8",
+           "img" : "http://cdn.youlxodxka.live/img/10.jpg",
+           "title" : "CCTV10科技"
+         },
+         {
+           "address" : "http://39.135.138.58:18890/PLTV/88888888/224/3221225628/index.m3u8",
+           "img" : "http://cdn.youlxodxka.live/img/11.jpg",
+           "title" : "CCTV11戏曲"
+         },
+         {
+           "address" : "http://39.135.138.58:18890/PLTV/88888888/224/3221225629/index.m3u8",
+           "img" : "http://cdn.youlxodxka.live/img/12.jpg",
+           "title" : "CCTV12社会与法"
+         },
+         {
+           "address" : "http://ncyd.jilu8.cn:80/PLTV/88888910/224/3221225638/index.m3u8",
+           "img" : "http://cdn.youlxodxka.live/img/13.jpg",
+           "title" : "CCTV13新闻"
+         },
+         {
+           "address" : "http://39.135.138.58:18890/PLTV/88888888/224/3221225639/index.m3u8",
+           "img" : "http://cdn.youlxodxka.live/img/14.jpg",
+           "title" : "CCTV14少儿"
+         },
+         {
+           "address" : "http://39.135.138.58:18890/PLTV/88888888/224/3221225641/index.m3u8",
+           "img" : "http://cdn.youlxodxka.live/img/15.jpg",
+           "title" : "CCTV15音乐"
+         },
+         {
+           "address" : "http://39.135.138.58:18890/PLTV/88888888/224/3221225908/index.m3u8",
+           "img" : "http://cdn.youlxodxka.live/img/17.jpg",
+           "title" : "CCTV17农村"
+         },
+         {
+           "address" : "http://hw-m-l.cztv.com/channels/lantian/channel01/1080p.m3u8",
+           "img" : "http://cdn.youlxodxka.live/img/zj.jpg",
+           "title" : "浙江卫视"
+         },
+         {
+           "address" : "http://39.135.138.58:18890/PLTV/88888888/224/3221225658/index.m3u8",
+           "img" : "http://cdn.youlxodxka.live/img/df.jpg",
+           "title" : "东方卫视"
+         },
+         {
+           "address" : "http://39.135.138.58:18890/PLTV/88888888/224/3221225704/index.m3u8",
+           "img" : "http://cdn.youlxodxka.live/img/hn.jpg",
+           "title" : "湖南卫视"
+         },
+         {
+           "address" : "http://39.135.138.58:18890/PLTV/88888888/224/3221225702/index.m3u8",
+           "img" : "http://cdn.youlxodxka.live/img/js.jpg",
+           "title" : "江苏卫视"
+         },
+         {
+           "address" : "http://live.xmcdn.com/live/1040/64.m3u8",
+           "img" : "http://cdn.youlxodxka.live/img/dt.png",
+           "title" : "环球资讯广播"
+         },
+         {
+           "address" : "http://live.xmcdn.com/live/1043/64.m3u8",
+           "img" : "http://cdn.youlxodxka.live/img/dt.png",
+           "title" : "中文环球广播"
+         },
+         {
+           "address" : "http://ls.qingting.fm/live/386.m3u8",
+           "img" : "http://cdn.youlxodxka.live/img/dt.png",
+           "title" : "中国之声"
+         }
+       ]
      **/
     
-    _playerView = [[WYPlayerViewObjc alloc] initWithFrame:self.view.bounds urlString:@"http://117.148.179.136/PLTV/88888888/224/3221231468/index.m3u8"];
+    _playerView = [[WYPlayerViewObjc alloc] initWithFrame:self.view.bounds urlString:@"http://39.135.138.58:18890/PLTV/88888888/224/3221225632/index.m3u8"];
     [self.view addSubview:_playerView];
     
     /* 准备播放 **/
